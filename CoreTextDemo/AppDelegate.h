a23cdb190bb09cc18f4414f4975ab6dc4a616a1c
zwy <zwy0615@qq.com>
2015-01-28 09:56:56 +0800

    Initial Commit

---
+++ CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by zwy on 15/1/28.
+//  Copyright (c) 2015年 zwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreTextDemo
+//
+//  Created by zwy on 15/1/28.
+//  Copyright (c) 2015年 zwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
